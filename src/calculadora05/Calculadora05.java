9cdc4c1880c2565771c6eb32337a7ca79acbab2d
Ander <redander0506@gmail.com>
2026-02-09 10:33:32 +0100

    Proyecto CalculadoraGIT05 clase Calculadora, método sumar, restar, multiplicar y dividir

--- src/calculadora05/Calculadora05.java
+++ src/calculadora05/Calculadora05.java
@@ -4,7 +4,16 @@ public class Calculadora05 {
 	public int sumar(int a, int b) { 
 		return a + b; 
 	}
+	
 	public int restar(int a, int b) { 
 		return a - b; 
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
+	public double dividir(int a, int b) {
+		return (double) a / b;
 	}
 }
